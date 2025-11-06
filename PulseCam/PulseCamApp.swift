caa46accc32d0f16f96bcbe761b9e8b10c29cd76
amirj <ajamshidi25@fed.idserve.net>
2025-11-06 10:19:46 +0100

    Initial Commit

---
+++ PulseCam/PulseCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseCamApp.swift
+//  PulseCam
+//
+//  Created by amirreza jamshidi on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
